4df17b31745ba3e40824f96932135330f86b618b
Chris <calandryll@gmail.com>
2018-06-27 13:49:02 -0400

    Updates

--- scripts/data_analysis.r
+++ scripts/data_analysis.r
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(slipper)
+library(boot)
 
 # Bootstrapping function to get the mean
 boot.mean = function(x, i){
@@ -8,6 +9,56 @@ boot.mean = function(x, i){
 
 # Read in data as a tibble
 cp = read_csv('csv/Coastal_plain_cleaned.csv')
+
+# Using the same setup as MD
+# Filter Good Condition and bootstrap
+cp.good = cp %>% 
+  filter(Biotic.Classification == 'Good Condition') %>%
+  select(-Site, -Watershed, -Biotic.Classification)
+
+good.boot = as.data.frame(apply(cp.good, 2, function(y){
+  b = boot(y, boot.mean, R = 20000, parallel = 'multicore', ncpus = 4);
+  c(boot.ci(b, type="perc", conf=0.8), mean(b$t))
+}))
+
+good.boot = as.data.frame(apply(cp.good, 2, function(y){
+  b = boot(y, boot.mean, R = 20000, parallel = 'multicore', ncpus = 4);
+  c(boot.ci(b, type="perc", conf=0.8)$percent[4], mean(b$t), boot::boot.ci(b, type="perc", conf=0.8)$percent[5])
+}))
+good.boot = good.boot %>% mutate(Condition = 'Good Condition') %>% select(Condition, CM:`Turbidity (Nephelometric)`)
+row.names(good.boot) = c('Lower 20% Limit', 'Mean', 'Upper 20% Limit')
+
+# Filter Moderately Degraded and bootstrap
+cp.mod = cp %>% 
+  filter(Biotic.Classification == 'Moderately Degraded') %>%
+  select(-Site, -Watershed, -Biotic.Classification)
+
+mod.boot = as.data.frame(apply(cp.mod, 2, function(y){
+  b = boot(y, boot.mean, R = 20000, parallel = 'multicore', ncpus = 4);
+  c(boot.ci(b, type="perc", conf=0.8)$percent[4], mean(b$t), boot::boot.ci(b, type="perc", conf=0.8)$percent[5])
+}))
+mod.boot = mod.boot %>% mutate(Condition = 'Moderarely Degraded') %>% select(Condition, CM:`Turbidity (Nephelometric)`)
+row.names(mod.boot) = c('Lower 20% Limit', 'Mean', 'Upper 20% Limit')
+
+# Filter Severely Degraded and bootstrap
+cp.severe = cp %>% 
+  filter(Biotic.Classification == 'Severely Degraded') %>%
+  select(-Site, -Watershed, -Biotic.Classification)
+
+severe.boot = as.data.frame(apply(cp.severe, 2, function(y){
+  b = boot(y, boot.mean, R = 20000, parallel = 'multicore', ncpus = 4);
+  c(boot.ci(b, type="perc", conf=0.8)$percent[4], mean(b$t), boot::boot.ci(b, type="perc", conf=0.8)$percent[5])
+}))
+severe.boot = severe.boot %>% mutate(Condition = 'Severely Degraded') %>% select(Condition, CM:`Turbidity (Nephelometric)`)
+row.names(severe.boot) = c('Lower 20% Limit', 'Mean', 'Upper 20% Limit')
+
+boot.cp = bind_rows(good.boot, mod.boot, severe.boot) %>% mutate(Stat = c('Lower 20% Limit', 'Mean', 'Upper 20% Limit', 'Lower 20% Limit', 'Mean', 'Upper 20% Limit', 'Lower 20% Limit', 'Mean', 'Upper 20% Limit')) %>% select(Condition, Stat, CM:`Turbidity (Nephelometric)`) %>% group_by(Condition) %>% nest()
+
+boot.cp2 = bind_rows(good.boot, mod.boot, severe.boot) %>% mutate(Stat = c('Lower', 'Mean', 'Upper', 'Lower', 'Mean', 'Upper', 'Lower', 'Mean', 'Upper')) %>% unite(Factor, Condition, Stat, sep = "_") %>% gather(Test, Data, CM:`Turbidity (Nephelometric)`)
+
+write_csv(boot.cp2, 'csv/Coastal_plain_bootstrap.csv')
+
+# Other ways?
 
 cp %>% 
   group_by(Biotic.Classification) %>% 
@@ -20,39 +71,16 @@ cp %>%
             mean = mean(value, na.rm = TRUE), 
             ci_high = quantile(value, 0.8))
 
-# Filter Good Condition and bootstrap
-cp.good = cp %>% 
+cp %>%
   filter(Biotic.Classification == 'Good Condition') %>%
-  select(-Site, -Watershed, -Biotic.Classification)
+  sample_n(size = 20000, replace = TRUE) %>%
+  summarise_all(funs(mean), na.rm = TRUE)
 
-good.boot = as.data.frame(apply(cp.good, 2, function(y){
-  b = boot::boot(y, boot.mean, R = 20000, parallel = 'multicore', ncpus = 4);
-  c(boot::boot.ci(b, type="perc", conf=0.8)$percent[4], mean(b$t), boot::boot.ci(b, type="perc", conf=0.8)$percent[5])
-}))
-row.names(good.boot) = c('Lower 20% Limit', 'Mean', 'Upper 20% Limit')
-good.boot = good.boot %>% mutate(Condition = 'Good Condition')
-
-# Filter Moderately Degraded and bootstrap
-cp.mod = cp %>% 
-  filter(Biotic.Classification == 'Moderately Degraded') %>%
-  select(-Site, -Watershed, -Biotic.Classification)
-
-mod.boot = as.data.frame(apply(cp.mod, 2, function(y){
-  b = boot::boot(y, boot.mean, R = 20000, parallel = 'multicore', ncpus = 4);
-  c(boot::boot.ci(b, type="perc", conf=0.8)$percent[4], mean(b$t), boot::boot.ci(b, type="perc", conf=0.8)$percent[5])
-}))
-mod.boot = mod.boot %>% mutate(Condition = 'Moderarely Degraded')
-row.names(mod.boot) = c('Lower 20% Limit', 'Mean', 'Upper 20% Limit')
-
-# Filter Severely Degraded and bootstrap
-cp.severe = cp %>% 
-  filter(Biotic.Classification == 'Severely Degraded') %>%
-  select(-Site, -Watershed, -Biotic.Classification)
-
-severe.boot = as.data.frame(apply(cp.severe, 2, function(y){
-  b = boot::boot(y, boot.mean, R = 20000, parallel = 'multicore', ncpus = 4);
-  c(boot::boot.ci(b, type="perc", conf=0.8)$percent[4], mean(b$t), boot::boot.ci(b, type="perc", conf=0.8)$percent[5])
-}))
-severe.boot = severe.boot %>% mutate(Condition = 'Severely Degraded')
-row.names(severe.boot) = c('Lower 20% Limit', 'Mean', 'Upper 20% Limit')
-
+cp %>%
+  group_by(Biotic.Classification) %>%
+  sample_n(size = 20000, replace = TRUE) %>%
+  select(-Site, -Watershed) %>%
+  summarize_all(funs(ci_low = quantile(., probs = 0.2),
+                     mean = mean,
+                     ci_high = quantile(., probs = 0.8)),
+                na.rm = TRUE, digits = 2)
